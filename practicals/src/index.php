0b147b49176410dbb5a36cf3eddbd42d49f19ad4
mishrasunny174 <mishrasunny174@gmail.com>
2019-04-25 19:12:10 +0530

    added index page

--- practicals/src/index.php
+++ practicals/src/index.php
@@ -1,3 +1,27 @@
 <?php
-	echo "Hello, World!";
+require_once "templates/header.php";
+$file_list = scandir(".");
+?>
 
+<div class="main">
+	<div class="header">
+		<h3>Practical List</h3>
+	</div>
+	<div class="links-block">
+		<?php
+		$file_list = array_filter($file_list, function ($file) {
+			if (preg_match("/^p\w*.php$/", $file))
+				return true;
+		});
+		foreach ($file_list as $file) {
+			$file_name = preg_replace("/.php/", "", $file);
+			$file_name = strtoupper($file_name);
+			echo "<a href='$file' class='links'>$file_name</a><br>\n";
+		}
+		?>
+	</div>
+</div>
+
+<?php
+require_once "templates/footer.php";
+?>
